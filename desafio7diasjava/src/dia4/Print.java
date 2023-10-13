c662c0d9463c5e84c21eb5650b2efbb49f6ee993
Arthur <arthur.fr.sc@gmail.com>
2023-10-13 10:09:03 -0300

    Dia 4

---
+++ desafio7diasjava/src/dia4/Print.java
@@ -0,0 +1,17 @@
+package dia4;
+//3. Write a Java program to print the following grid.
+public class Print {
+	    public static void main(String[] args) {
+	        int horizontal = 10;
+	        int vertical = 10;
+
+	        for (int i=0;i<horizontal;i++){
+	            for (int j=0;j<vertical;j++) {
+	                System.out.print("- ");
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
+
+
